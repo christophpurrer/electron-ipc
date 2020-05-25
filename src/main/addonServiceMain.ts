1f887e3f5a3484ba497b2e4fd4f1e65387d269d4
Christoph Purrer <chpurrer@fb.com>
2020-05-25 10:10:19 +0200

    Add ipcUtils and update README

--- src/main/addonServiceMain.ts
+++ src/main/addonServiceMain.ts
@@ -1,9 +1,9 @@
-import { IpcMainInvokeEvent } from "electron";
 import { loadAndWrapAddon, Addon, SystemInfo } from "../shared/addon";
 import { AddonService } from "../shared/addonService";
 import { IpcMain } from "electron";
+import { registerIpcChannels, IpcService } from "../shared/ipcUtils";
 
-export class AddonServiceMain implements AddonService {
+export class AddonServiceMain implements AddonService, IpcService {
   ipcMain: IpcMain;
   addon: Addon;
   userId: string | null = null;
@@ -11,25 +11,7 @@ export class AddonServiceMain implements AddonService {
     console.log("addonService constructor ...");
     this.ipcMain = ipcMain;
     this.addon = loadAndWrapAddon();
-    const methods = Object.getOwnPropertyNames(
-      Object.getPrototypeOf(this)
-    ).filter(
-      (method) =>
-        method &&
-        method !== "constructor" &&
-        (this as any)[method] !== undefined &&
-        typeof (this as any)[method] === "function"
-    );
-    methods.forEach((method: string) => {
-      console.log(`addonService register: ${method}(...) on ipcMain`);
-      this.ipcMain.handle(
-        method,
-        async (_event: IpcMainInvokeEvent, args: any) => {
-          const f = (this as any)[method];
-          return f.call(this, ...args);
-        }
-      );
-    });
+    registerIpcChannels(this);
   }
   getSystemInfo(threadId: number, feature: string): Promise<SystemInfo> {
     return this.addon.getSystemInfo(threadId, feature);
---
+++ src/shared/ipcUtils.ts
@@ -0,0 +1,24 @@
+import { IpcMain } from "electron";
+
+export type IpcService = {
+  ipcMain: IpcMain;
+};
+
+export function registerIpcChannels(obj: IpcService) {
+  // get object methods
+  const methods = Object.getOwnPropertyNames(Object.getPrototypeOf(obj)).filter(
+    (method) =>
+      method &&
+      method !== "constructor" &&
+      (obj as any)[method] !== undefined &&
+      typeof (obj as any)[method] === "function"
+  );
+  // register object methods to respond to ipc requests
+  methods.forEach((method: string) => {
+    console.log(`addonService register: ${method}(...) on ipcMain`);
+    obj.ipcMain.handle(method, async (_event: any, args: any) => {
+      const f = (obj as any)[method];
+      return f.call(obj, ...args);
+    });
+  });
+}
